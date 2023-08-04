db16c90e2171011da152772ad5999626ce0760e2
ryan-dia <cjfdnjs158@naver.com>
2023-08-04 20:50:04 +0900

    chore(ts): fix typo

--- packages/eslint-config-hijacking-ts/index.js
+++ packages/eslint-config-hijacking-ts/index.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['plugin:@typescript-eslint/recommended'].concat(
-    ['eslint-config-hijacking-js', './rules/style', './rules/types'].map(require.resolve)
+    ['eslint-config-hijacking-js', './rules/styles', './rules/types'].map(require.resolve)
   ),
   parserOptions: {
     project: true,
