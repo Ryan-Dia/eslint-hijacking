6b391e927e7b3386fa4e7d5008dbd2a5c5cea535
ryan-dia <cjfdnjs158@naver.com>
2023-09-22 23:58:42 +0900

    feat(JS): add es6

---
+++ packages/eslint-config-hijacking-js/rules/es6.js
@@ -0,0 +1,92 @@
+module.exports = {
+  rules: {
+    // Disallow modifying variables that are declared using const
+    // The "extends": "eslint:recommended" property in a configuration file enables this rule
+    // https://eslint.org/docs/latest/rules/no-const-assign.html
+    'no-const-assign': 'error',
+
+    // Require method and property shorthand syntax for object literals
+    // https://eslint.org/docs/rules/object-shorthand
+    'object-shorthand': [
+      'error',
+      'always',
+      {
+        ignoreConstructors: false,
+        avoidQuotes: true,
+      },
+    ],
+
+    // Prefer destructuring from arrays and objects
+    // https://eslint.org/docs/rules/prefer-destructuring
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: true,
+          object: false,
+        },
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+
+    // Suggest using template literals instead of string concatenation
+    // https://eslint.org/docs/rules/prefer-template
+    'prefer-template': 'error',
+
+    // Enforce usage of spacing in template strings
+    // https://eslint.org/docs/rules/template-curly-spacing
+    'template-curly-spacing': 'error',
+
+    // Use rest parameters instead of arguments
+    // https://eslint.org/docs/rules/prefer-rest-params
+    'prefer-rest-params': 'error',
+
+    // Suggest using the spread syntax instead of .apply()
+    // https://eslint.org/docs/rules/prefer-spread
+    'prefer-spread': 'error',
+
+    // Suggest using arrow functions as callbacks
+    // https://eslint.org/docs/latest/rules/prefer-arrow-callback.html
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: false,
+        allowUnboundThis: true,
+      },
+    ],
+
+    // Require space before/after arrow function's arrow
+    // https://eslint.org/docs/rules/arrow-spacing
+    'arrow-spacing': ['error', { before: true, after: true }],
+
+    // Require parens in arrow function arguments
+    // https://eslint.org/docs/rules/arrow-parens
+    'arrow-parens': ['error', 'always'],
+
+    // Enforces no braces where they can be omitted
+    // https://eslint.org/docs/rules/arrow-body-style
+    // TODO: enable requireReturnForObjectLiteral?
+    'arrow-body-style': [
+      'error',
+      'as-needed',
+      {
+        requireReturnForObjectLiteral: false,
+      },
+    ],
+
+    // Disallow arrow functions where they could be confused with comparisons
+    // https://eslint.org/docs/rules/no-confusing-arrow
+    'no-confusing-arrow': [
+      'error',
+      {
+        allowParens: true,
+      },
+    ],
+  },
+};
